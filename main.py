33f12e4b0d146fd773469a24d356e0a68657c1af
justpetrovich <dmitry.ivanov.od@gmail.com>
2025-07-13 16:09:39 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI, WebSocket
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from langchain_ollama import ChatOllama
+import logging
+import json
+import uuid
+
+ALLOWED_ORIGINS = [
+    "*",
+    "http://localhost:3000/"
+]
+MODEL_NAME = "llama3.1"
+
+logging.basicConfig(filename='app.log', level=logging.INFO)
+
+llm = ChatOllama(model=MODEL_NAME)
+
+app = FastAPI(title="Simple AI chatbot", version="0.0.1")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[str(origin) for origin in ALLOWED_ORIGINS],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def root():
+    return HTMLResponse("""
+    <script>
+        window.location.href = '/static/index.html';
+    </script>
+    """)
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            req = await websocket.receive_text()
+            data = json.loads(req)
+            request_id = data['id']
+            user_msg = data['content']
+            response_id = str(uuid.uuid4())[:6]
+            await websocket.send_text(json.dumps({
+                'status': 'msg-received',
+                'role': 'user',
+                'userMsgId': request_id,
+                'userMsg': user_msg,
+            }))
+            logging.info(f"User Query: {user_msg}")
+            for chunks in llm.stream(user_msg):
+                await websocket.send_text(json.dumps({
+                    'status': 'response-stream',
+                    'role': 'assistant',
+                    'userMsgId': request_id,
+                    'assistantMsgId': response_id,
+                    'assistantMsg': str(chunks.content),
+                }))
+            await websocket.send_text(json.dumps({
+                'status': 'end',
+                'userMsgId': request_id,
+            }))
+    except Exception as e:
+        error_msg = f'WebSocket error: {str(e)}'
+        logging.error(error_msg)
+        print(error_msg)
+    finally:
+        await websocket.close()
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)````
